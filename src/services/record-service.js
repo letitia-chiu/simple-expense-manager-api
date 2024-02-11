49e3880e2af2f734b435b2d8131ca91eb0e55db1
letitia-chiu <hsuan.acc@outlook.com>
2024-02-11 22:40:04 +0800

    add date attribute to postRecord & patchRecord

--- src/services/record-service.js
+++ src/services/record-service.js
@@ -26,7 +26,7 @@ const recordService = {
 
   postRecord: async (req, cb) => {
     try {
-      const { title, amount, categoryId } = req.body
+      const { title, amount, categoryId, date } = req.body
 
       // Validate user input
       if (!title) throw new HttpError(400, 'Title is required')
@@ -46,7 +46,8 @@ const recordService = {
         amount,
         categoryId: categoryId || null,
         userId: req.user.id,
-        isIncome: req.isIncome
+        isIncome: req.isIncome,
+        date: date || new Date()
       })
 
       // Return data
@@ -88,24 +89,27 @@ const recordService = {
 
   patchRecord: async (req, cb) => {
     try {
-      const { title, amount, categoryId } = req.body
+      const { title, amount, categoryId, date } = req.body
 
       // Validate user input
       if (!title) throw new HttpError(400, 'Title is required')
       if (!amount) throw new HttpError(400, 'Amount is required')
 
+      // Check if record exists & belongs to user
+      const record = await Record.findByPk(req.params.id)
+      if (!record) throw new HttpError(404, 'Record not found')
+      if (record.userId !== req.user.id) throw new HttpError(403, 'Permission denied')
+
+      // Get & check category
       let category = null
       if (categoryId) {
         category = await Category.findByPk(categoryId)
         if (!category || category.isIncome !== req.isIncome || category.userId !== req.user.id) {
           throw new HttpError(400, 'Invalid category')
         }
+      } else if (record.categoryId) {
+        category = await Category.findByPk(record.categoryId)
       }
-
-      // Check if record exists & belongs to user
-      const record = await Record.findByPk(req.params.id)
-      if (!record) throw new HttpError(404, 'Record not found')
-      if (record.userId !== req.user.id) throw new HttpError(403, 'Permission denied')
 
       // Check if isIncome match
       if (record.isIncome !== req.isIncome) {
@@ -117,7 +121,8 @@ const recordService = {
       await record.update({
         title,
         amount,
-        categoryId: categoryId || null
+        ...categoryId ? { categoryId } : {},
+        ...date? { date } : {}
       })
 
       // Return data
